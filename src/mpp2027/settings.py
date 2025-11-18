2b1d63fb6af7d962e953a92a543339167434e5a6
Papyjulio <julien.python.test@gmail.com>
2025-11-18 05:53:11 +0100

    Cleanup: Suppression de Cloudinary, préparation pour Railway

--- src/mpp2027/settings.py
+++ src/mpp2027/settings.py
@@ -45,8 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
 
     'django.contrib.staticfiles',
-    'cloudinary_storage',
-    'cloudinary',
     # --- BLOC POUR ALLAUTH ---
     'django.contrib.sites',  # Requis par allauth
     'allauth',
@@ -220,26 +218,11 @@ LOGOUT_REDIRECT_URL = 'blog:liste-articles'
 # Site ID pour allauth
 SITE_ID = 1
 
-# =============================================================================
-# CONFIGURATION DU STOCKAGE MÉDIA (CLOUDINARY)
-# =============================================================================
-#
-# On lit les 3 clés secrètes depuis nos variables d'environnement
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': config('CLOUDINARY_API_KEY'),
-    'API_SECRET': config('CLOUDINARY_API_SECRET'),
-}
 
-# On dit à Django d'utiliser Cloudinary pour TOUS les fichiers média
-MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-"""
 # Fichiers Media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
-"""
+
 # Pagination du blog
 BLOG_ARTICLES_PAR_PAGE = 5
 
